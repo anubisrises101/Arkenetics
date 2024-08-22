e0601ba8007378b1d3e0c7af5d0914e304f60f2c
Phillip McKenzie <pmckenzie6387@gmail.com>
2024-08-22 13:14:34 -0400

    Start of project

---
+++ server.js
@@ -0,0 +1,36 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const app = express();
+
+// Set the port from environment variable or default to 3000
+const port = process.env.PORT ? process.env.PORT : "3000";
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
+// serves static assets to the browser
+app.use(express.static('public'))
+// Middleware to parse URL-encoded data from forms
+app.use(express.urlencoded({ extended: false }));
+// Middleware for using HTTP verbs such as PUT or DELETE
+app.use(methodOverride("_method"));
+
+
+app.get('/', async (req, res) => {
+    res.render('home.ejs');
+  });
+
+
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
