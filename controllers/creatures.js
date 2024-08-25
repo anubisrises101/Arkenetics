e54e1659b993a08fc71c0611e619042ef855134e
Phillip McKenzie <pmckenzie6387@gmail.com>
2024-08-25 18:28:51 -0400

    Refactoring files

--- controllers/creatures.js
+++ controllers/creatures.js
@@ -1,21 +1,25 @@
+const UserCreature = require('../models/userCreature');
 const express = require('express');
 const router = express.Router();
 const Creature = require('../models/creature');
 
+// all  paths start with '/usercreatures'
 
-//get all creatures to pass to the select feature
-router.get('/', async (req, res) => {
-    const allCreatures = await Creature.find({});
-    res.render('users/index.ejs', {
-        creatures: allCreatures,
-    });
-  });
-
-router.get('/new', async (req, res) => {
-    const allCreatures = await Creature.find({});
-    res.render('users/new.ejs', {
-        creatures: allCreatures,
-    });
+//GET /users/creatures (index functionality)
+router.get('/:userId', async (req, res) => {
+    const creatures = await UserCreature.find({user: req.params.userId});
+    res.render('users/index.ejs', {creatures});
 });
 
-  module.exports = router;
+// router.new('/', async (req, res) => {
+//     const creatureId = req.body.creatures
+//     const creatureObj = await Creature.findById(creatureId);
+//     delete creatureObj._id;
+//     creatureObj.user = req.user._id;
+//     const userCreature = UserCreature.create(creatureObj);
+//     res.redirect(`/users/creatures/${req.user._id}`)
+// })
+
+
+//hhow to create a userCreature obj
+module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const session = require('express-session');
 // const authController = require('./controllers/auth.js')
 
-
 const port = process.env.PORT ? process.env.PORT : "3000";
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -17,13 +16,9 @@ mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-// Morgan for logging HTTP requests
 app.use(morgan('dev'));
-// serves static assets to the browser
 app.use(express.static('public'))
-// Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
-// Middleware for using HTTP verbs such as PUT or DELETE
 app.use(methodOverride("_method"));
 app.use(
   session({
@@ -34,25 +29,17 @@ app.use(
 );
 
 const addUserToReqAndLocals = require('./middleware/addUserToReqAndLocals.js');
+app.use(addUserToReqAndLocals);
+
 const ensureLoggedIn = require('./middleware/ensureLoggedIn.js');
 
-
-
-app.use(addUserToReqAndLocals);
-app.use('/auth', require('./controllers/auth'));
-// app.use(ensureLoggedIn)
-app.use('/users/creatures', ensureLoggedIn, require('./controllers/users.js'))
-app.use('/creatures', require('./controllers/creatures.js'))
 // Routes
-
-
-
+app.use('/auth', require('./controllers/auth'));
+app.use('/creatures', ensureLoggedIn, require('./controllers/creatures.js'))
 
 app.get('/', async (req, res) => {
     res.render('home.ejs');
 });
-
-
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
